9c87fa59521811d733aaf92b67ef98137e313984
Zahid Tokur <zahidtokur@gmail.com>
2020-10-09 14:17:14 +0300

    Custom permission exceptions
    Catching an exception that raises when request doesn't contain an Authorization header.

--- event/permissions.py
+++ event/permissions.py
@@ -7,4 +7,9 @@ class IsOwner(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        return obj.auth_token.__str__() == request.headers['Authorization']
+        try:
+            token = request.headers['Authorization']
+        except KeyError:
+            return False
+
+        return obj.auth_token.__str__() == token
